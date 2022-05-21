aca5273b679ac47cb31d4a6239d335fab9b44933
VigBick <vigbick@gmail.com>
2022-05-21 04:58:41 -0600

    actualización dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -40,37 +40,13 @@
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
-    import Welcome from '@/Jetstream/Welcome.vue'
-
+   
     export default defineComponent({
         components: {
             AppLayout,
         },
-        /*props: {
-            news_by_category;
-            }*/
-
-            /*<table class="min-w-full divide-y divide-gray-200 w-full">
-                <thead>
-                    <tr>
-                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Categoría
-                        </th>
-                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Estatus
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                    <tr v-for="categoria in categorias" :key="categoria.id">
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                            {{categoria.category_name}}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                            {{categoria.category_name}}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>*/
+        props: {
+            news_by_category: Array
+        }
     })
 </script>
--- routes/web.php
+++ routes/web.php
@@ -27,19 +27,17 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    /*$news_by_category = DB::table('notas')
+    $news_by_category = DB::table('notas')
         ->join('categories', 'notas.categories_id', "=", 'categories.id')
         ->where('users_id', Auth::id())
         ->select(DB::raw('category_name, count(categories.id) as total'))
         ->groupBy('category_name','categories.id')
         ->orderBy('total','desc')
-        ->get();*/
-    return Inertia::render('Dashboard');
-})->name('dashboard');
-/*    return Inertia::render('Dashboard',[
+        ->get();
+    return Inertia::render('Dashboard',[
     'news_by_category' => $news_by_category
 ]);
-})->name('dashboard');*/
+})->name('dashboard');
 
 Route::resource('noticias', NotaController::class);
 
